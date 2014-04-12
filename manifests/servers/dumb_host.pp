5e20320c18cc5ea18e7c7ac260e8a7485c2b32a3
Adam Jacques <adam@adamjacques.com>
2014-04-12 12:37:52 -0400

    Relay host server configuration supports passwords and new configuration structure

--- manifests/servers/dumb_host.pp
+++ manifests/servers/dumb_host.pp
@@ -1,9 +1,34 @@
 class postfix::servers::dumb_host (
-	$relay_host = $::postfix_relayhost
+	$relay_host = undef,
+	$username = undef,
+	$password = undef
 ) {
 	postfix::cf_property {'postfix_relayhost':
 		variable => 'relayhost',
 		value => $relay_host
+	}
+	if $username != undef {
+		postfix::cf_property {'postfix_smtp_sasl_auth_enable':
+			variable => 'smtp_sasl_auth_enable',
+			value => 'yes'
+		}
+		postfix::cf_property {'postfix_sasl_password_map':
+			variable => 'smtp_sasl_password_maps',
+			value => 'hash:${config_directory}/relay_password'
+		}
+		exec {'postfix_relay_password_postmap':
+			refreshonly => true,
+			command => "/usr/sbin/postmap ${postfix::config::config_dir}/relay_password",
+			require => File["${postfix::config::config_dir}/relay_password"]
+		}
+
+		file {"${postfix::config::config_dir}/relay_password":
+			owner => 'root',
+			group => 'root',
+			mode => '400',
+			notify => Exec['postfix_relay_password_postmap'],
+			content => "${relay_host} ${username}:${password}"
+		}
 	}
 
 	postfix::server_port {'pickup':
